b71f4499cd14ad5dedf6fc488f4d69b720f7ed83
Caleb Condor <calebcondor553@gmail.com>
2026-08-25 16:59:38 -0500

    chore(index): rename page title to Notarizalo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-  <title>Notarize — Notaría en línea</title>
+  <title>Notarizalo</title>
 
   <!-- Tailwind CSS CDN -->
   <script src="https://cdn.tailwindcss.com"></script>
